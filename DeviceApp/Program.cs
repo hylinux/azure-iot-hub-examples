ae4a4e62da340b03774ec03a0be9512fc6cc51bc
HongWei Guo <hongweig@163.com>
2022-12-27 21:48:18 +0800

    put a device app into example`

---
+++ DeviceApp/Program.cs
@@ -0,0 +1,24 @@
+using DeviceApp;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
+    {
+        options.ServiceName = "IoT Device App";
+    })
+    .ConfigureServices(services =>
+    {
+        LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(services);
+
+        services.AddHostedService<DeviceService>();
+    })
+    .ConfigureLogging((context, logging) =>
+    {
+        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+    })
+    .Build();
+
+await host.RunAsync();
+
---
+++ ExportAndImportIdentity/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Devices;
+
+RegistryManager registryManager = RegistryManager.CreateFromConnectionString(
+    @""
+);
+
+JobProperties exportJob = await registryManager.ExportDevicesAsync(
+    @"",
+    false
+);
+
+
+while(true)
+{
+    exportJob = await registryManager.GetJobAsync(exportJob.JobId);
+
+    if (exportJob.Status == JobStatus.Completed ||
+    exportJob.Status == JobStatus.Failed ||
+    exportJob.Status == JobStatus.Cancelled)
+    {
+        // Job has finished executing
+        break;
+    }
+
+    await Task.Delay(TimeSpan.FromSeconds(5));
+}
